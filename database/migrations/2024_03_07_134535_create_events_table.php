055532fff4c9c02037279b63e81545cfd76444f7
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 14:48:03 +0100

    migration event

--- database/migrations/2024_03_07_134535_create_events_table.php
+++ database/migrations/2024_03_07_134535_create_events_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('image');
+            $table->string('date');
+            $table->string('location');
+            $table->integer('availablePlaces')->default(0);
+            $table->enum('ReservationType', ['automatically','manually'])->default('automatically');
+            $table->enum('status', ['0','1'])->default('0');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
