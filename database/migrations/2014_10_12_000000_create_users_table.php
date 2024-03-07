381719af22ec004bde56cc50ba93a6ac61414aaa
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 16:16:39 +0100

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('role', ['utilisateur', 'organisateur', 'admin'])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
